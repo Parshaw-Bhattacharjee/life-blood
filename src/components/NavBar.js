986437cf134aad0e8a51363e46aa14cec6205cda
Rohan Mondal <mondalrohan108@gmail.com>
2022-05-05 10:05:22 +0530

    fixed nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,35 @@
-import { React } from "react";
-import { AiOutlineUser } from "react-icons/ai";
-import { FaHospitalAlt } from "react-icons/fa";
-import { RiBankFill } from "react-icons/ri";
-import { Link } from "react-router-dom";
+import { React } from 'react';
+import { AiOutlineUser } from 'react-icons/ai';
+import { FaHospitalAlt } from 'react-icons/fa';
+import { RiBankFill } from 'react-icons/ri';
+import { Link, useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
-    <div className="fixed w-full flex justify-between items-center p-4">
-      <div className="">
-        <h1>
-          <Link to={"/"}>Life-Blood</Link>
-        </h1>
+    <div className='fixed top-0 left-0 right-0 w-full flex justify-between items-center p-4'>
+      <div className=''>
+        <img
+          onClick={() => navigate('/')}
+          className='h-20 rounded-lg cursor-pointer'
+          src='https://res.cloudinary.com/donqbxlnc/image/upload/v1651724100/Life_Blood_tr_cropped_gvqcyj.png'
+          alt=''
+        />
       </div>
       <nav>
-        <ul className="flex gap-10 p-6 bg-black/50 text-white rounded-3xl md:text-2xl">
+        <ul className='flex gap-8 p-4 bg-black/50 text-white rounded-3xl md:text-2xl'>
           <li>
-            <Link to={"/hospital"}>
+            <Link to={'/hospital'}>
               <FaHospitalAlt />
             </Link>
           </li>
           <li>
-            <Link to={"/blood_bank"}>
+            <Link to={'/blood_bank'}>
               <RiBankFill />
             </Link>
           </li>
           <li>
-            <Link to={"/donor"}>
+            <Link to={'/donor'}>
               <AiOutlineUser />
             </Link>
           </li>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 const LandingPage = () => {
   return (
-    <div className="bg-blood-donation-image w-full h-screen bg-cover bg-center items-center flex px-4"></div>
+    <div className='bg-blood-donation-image w-full h-screen bg-cover bg-center items-center flex px-4'></div>
   );
 };
 
