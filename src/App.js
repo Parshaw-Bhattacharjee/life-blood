984bed677234cd0d6f3ccd9a4c58de04d81ad989
Rohan Mondal <mondalrohan108@gmail.com>
2022-05-05 10:05:16 +0530

    fixed nav

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import LandingPage from "./pages/LandingPage";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
-import Donor from "./pages/Donor";
-import BloodBank from "./pages/BloodBank";
-import Hospital from "./pages/Hospital";
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import LandingPage from './pages/LandingPage';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Donor from './pages/Donor';
+import BloodBank from './pages/BloodBank';
+import Hospital from './pages/Hospital';
 
 function App() {
   return (
     <>
       <NavBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/donor" element={<Donor />} />
-          <Route path="/blood_bank" element={<BloodBank />} />
-          <Route path="/hospital" element={<Hospital />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/donor' element={<Donor />} />
+        <Route path='/blood_bank' element={<BloodBank />} />
+        <Route path='/hospital' element={<Hospital />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+
       <Footer />
     </>
   );
