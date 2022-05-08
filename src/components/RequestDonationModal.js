b43209791e146b15f9c984de628010a48f967072
ParshawBhattacharjee <parshawbhattacharjee@gmail.com>
2022-05-08 07:28:00 +0530

    touch up

---
+++ src/components/RequestDonationModal.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { AiFillCloseCircle } from "react-icons/ai";
+
+const RequestDonationModal = () => {
+  const [showModal, setShowModal] = React.useState(false);
+  return (
+    <>
+      <button
+        className="bg-rose-500 text-white active:bg-rose-600 font-bold uppercase text-sm px-6 py-3 rounded-lg shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Request Donation
+      </button>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Request Blood Donation
+                  </h3>
+                  <button
+                    className="pl-4 py-1 ml-auto bg-transparent border-0  float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <AiFillCloseCircle />
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <form action="" className="flex flex-col space-y-4">
+                    <div>
+                      <label htmlFor="" className="text-sm">
+                        Name
+                      </label>
+                      <input
+                        type={"text"}
+                        placeholder="Your Name"
+                        className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-rose-300"
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="" className="text-sm">
+                        Email ID
+                      </label>
+                      <input
+                        type={"email"}
+                        placeholder="example@gmail.com"
+                        className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-rose-300"
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="" className="text-sm">
+                        Message
+                      </label>
+                      <textarea
+                        placeholder="Message"
+                        rows={"3"}
+                        className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-rose-300"
+                      />
+                    </div>
+                  </form>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="inline-block self-end bg-red-500 text-white font-bold rounded-lg px-6 py-2 uppercase text-sm"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Request
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default RequestDonationModal;
--- src/pages/BloodBank.js
+++ src/pages/BloodBank.js
@@ -1,15 +1,17 @@
 import React from "react";
 import BankRequestSection from "../components/BankRequestSection";
 import Chart from "../components/Chart";
+import RequestDonationModal from "../components/RequestDonationModal";
 
 const BloodBank = () => {
   return (
     <div>
-      <div className="flex w-full min-h-screen justify-center items-center">
+      <div className="flex flex-col w-full min-h-screen justify-center items-center">
         <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 w-full max-w-4xl p-8 sm:p-12 rounded-xl overflow-hidden">
           <BankRequestSection />
           <Chart />
         </div>
+        <RequestDonationModal />
       </div>
     </div>
   );
