c91605628e248851fa75d7a8c8e3bf04a08f1ca4
ParshawBhattacharjee <parshawbhattacharjee@gmail.com>
2022-05-28 16:38:50 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
@@ -14,16 +13,6 @@ import SignupHospital from "./pages/Auth/SignupHospital";
 import SignupBloodBank from "./pages/Auth/SignupBloodBank";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-=======
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import BloodBank from './pages/BloodBank/BloodBank';
-import Donor from './pages/Donor/Donor';
-import Hospital from './pages/Hospital/Hospital';
-import LandingPage from './pages/LandingPage';
-import LoginDonor from './pages/Auth/LoginDonor';
-import Login
->>>>>>> 32d1292460ca9429e7dc546bd1c4ea0f3636ddcb
 
 function App() {
   return (
@@ -31,29 +20,29 @@ function App() {
       <NavBar />
 
       <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/donor' element={<Donor />} />
-        <Route path='/bloodbank' element={<BloodBank />} />
-        <Route path='/hospital' element={<Hospital />} />
-
-        {/* <Route path='/contact' element={<Contact />} /> */}
-        <Route path='/login/donor' element={<LoginDonor user={'donor'} />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/donor" element={<Donor />} />
+        <Route path="/bloodbank" element={<BloodBank />} />
+        <Route path="/hospital" element={<Hospital />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login/donor" element={<LoginDonor user={"donor"} />} />
         <Route
-          path='/login/bloodbank'
-          element={<LoginBloodBank user={'bloodbank'} />}
+          path="/login/bloodbank"
+          element={<LoginBloodBank user={"bloodbank"} />}
         />
         <Route
-          path='/login/hospital'
-          element={<LoginHospital user={'donor'} />}
+          path="/login/hospital"
+          element={<LoginHospital user={"donor"} />}
         />
-        <Route path='/signup/donor' element={<SignupDonor user={'donor'} />} />
+        <Route path="/signup/donor" element={<SignupDonor user={"donor"} />} />
         <Route
-          path='/signup/bloodbank'
-          element={<SignupBloodBank user={'bloodbank'} />}
+          path="/signup/bloodbank"
+          element={<SignupBloodBank user={"bloodbank"} />}
         />
         <Route
-          path='/signup/hospital'
-          element={<SignupHospital user={'hospital'} />}
+          path="/signup/hospital"
+          element={<SignupHospital user={"hospital"} />}
         />
       </Routes>
 
--- src/pages/Hospital/Hospital.js
+++ src/pages/Hospital/Hospital.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import AcceptanceHistory from "./components/AcceptanceHistory";
 import BloodRequestForm from "./components/BloodRequestForm";
@@ -9,18 +8,6 @@ const Hospital = () => {
     <div className="flex flex-col p-2 justify-center items-center">
       <div className="flex w-full min-h-screen justify-center items-center">
         <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 w-full max-w-4xl p-8 sm:p-12 rounded-xl overflow-hidden">
-=======
-import React from 'react';
-import HospitalRequestSection from './components/HospitalRequestSection';
-import BloodRequestForm from './components/BloodRequestForm';
-import AcceptanceHistory from './components/AcceptanceHistory';
-
-const Hospital = () => {
-  return (
-    <div className='flex flex-col p-2 justify-center items-center'>
-      <div className='flex w-full min-h-screen justify-center items-center'>
-        <div className='flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 w-full max-w-4xl p-8 sm:p-12 rounded-xl overflow-hidden'>
->>>>>>> 32d1292460ca9429e7dc546bd1c4ea0f3636ddcb
           <HospitalRequestSection />
           <BloodRequestForm />
         </div>
