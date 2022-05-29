0df988f5f0ae95aa11ba59edb2bb1c9a0107ec53
ParshawBhattacharjee <parshawbhattacharjee@gmail.com>
2022-05-29 16:32:55 +0530

    fix carousel

--- src/components/CarouselSection.js
+++ src/components/CarouselSection.js
@@ -3,26 +3,26 @@ import React from "react";
 const CarouselSection = () => {
   return (
     <div>
-      <div classname="grid grid-cols-3 gap-4 items-center p-10">
-        <div classname="mb-4">
+      <div className="grid grid-cols-3 gap-4 items-center p-10">
+        <div className="mb-4">
           <img
             src="https://www.wockhardthospitals.com/wp-content/uploads/2019/11/2971635.jpg"
-            classname="min-w-full h-auto rounded-lg"
+            className="min-w-full h-auto rounded-lg"
             alt=""
           />
         </div>
 
-        <div classname="mb-4">
+        <div className="mb-4">
           <img
             src="https://www.fortishealthcare.com/blog/wp-content/uploads/2014/07/blog_24.png"
-            classname="min-w-full h-auto rounded-full"
+            className="min-w-full h-auto rounded-full"
             alt=""
           />
         </div>
-        <div classname="mb-4">
+        <div className="mb-4">
           <img
             src="https://www.1mg.com/articles/wp-content/uploads/2019/06/blood-donation.jpg"
-            classname="min-w-full h-auto rounded-full"
+            className="min-w-full h-auto rounded-full"
             alt=""
           />
         </div>
