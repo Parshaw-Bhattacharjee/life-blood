738a33d9e210b60f76a425e8aeef93ec7e25a3d3
ParshawBhattacharjee <parshawbhattacharjee@gmail.com>
2022-06-02 07:27:38 +0530

    changed

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAGzAYaVyYLw8cqs3vz0I5iXY6OuZvQjC0",
+  authDomain: "life-blood-de5c6.firebaseapp.com",
+  projectId: "life-blood-de5c6",
+  storageBucket: "life-blood-de5c6.appspot.com",
+  messagingSenderId: "692220667701",
+  appId: "1:692220667701:web:76ed515d19ba9ba2120e7c",
 };
 
 // Initialize Firebase
--- src/pages/BloodBank/BloodBank.js
+++ src/pages/BloodBank/BloodBank.js
@@ -1,31 +1,56 @@
-import React from 'react';
-import BankRequestSection from './components/BankRequestSection';
+import React from "react";
+import BankRequestSection from "./components/BankRequestSection";
 
-import RequestDonationModal from './components/RequestDonationModal';
-import { useEffect, useState } from 'react';
-import { useAuth } from '../../contexts/auth-context';
-import { collection, onSnapshot, query } from 'firebase/firestore';
-import { db } from '../../firebase';
-import ShowChart from '../../components/Chart';
+import RequestDonationModal from "./components/RequestDonationModal";
+import { useEffect, useState } from "react";
+import { useAuth } from "../../contexts/auth-context";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { db } from "../../firebase";
+import ShowChart from "../../components/Chart";
+import PendingRequests from "./components/PendingRequests";
 
 const BloodBank = () => {
   const [hospitalRequests, setHospitalRequests] = useState([]);
+  const [DonorRequests, setDonorRequests] = useState([]);
   const { user } = useAuth();
   const [bloodData, setBloodData] = useState({
     labels: [],
     datasets: [
       {
-        label: 'Dataset 1',
+        label: "Dataset 1",
         data: [],
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
     ],
   });
-  const [bloodDataChanged, isBloodDataChanged] = useState(false);
+  const [bloodDataDecrease, isBloodDataDecrease] = useState(false);
+  const [bloodDataIncrease, isBloodDataIncrease] = useState(false);
 
   useEffect(() => {
     (async () => {
-      const q = query(collection(db, 'blood_bank'));
+      const q = query(collection(db, "blood_bank"));
+      onSnapshot(q, (data) => {
+        const dataObj = data.docs[0].data();
+
+        setBloodData({
+          labels: Object.keys(dataObj.bloodData).sort((a) => a + 1),
+          datasets: [
+            {
+              label: "Blood Storage Data",
+              data: Object.keys(dataObj.bloodData)
+                .sort((a) => a + 1)
+                .map((el) => dataObj.bloodData[el]),
+              backgroundColor: "rgba(255, 99, 132, 0.5)",
+            },
+          ],
+        });
+      });
+    })();
+  }, [bloodDataIncrease]);
+
+  useEffect(() => {
+    (async () => {
+      const q = query(collection(db, "blood_bank"));
       onSnapshot(q, (data) => {
         const dataObj = data.docs[0].data();
 
@@ -33,22 +58,22 @@ const BloodBank = () => {
           labels: Object.keys(dataObj.bloodData).sort((a, b) => a - b),
           datasets: [
             {
-              label: 'Blood Storage Data',
+              label: "Blood Storage Data",
               data: Object.keys(dataObj.bloodData)
                 .sort((a, b) => a - b)
                 .map((el) => dataObj.bloodData[el]),
-              backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              backgroundColor: "rgba(255, 99, 132, 0.5)",
             },
           ],
         });
       });
     })();
-  }, [bloodDataChanged]);
+  }, [bloodDataDecrease]);
 
   useEffect(() => {
     if (user) {
       (async () => {
-        const q = query(collection(db, 'requests'));
+        const q = query(collection(db, "requests"));
         onSnapshot(q, (data) => {
           const dataObj = data.docs[0].data();
           setHospitalRequests(
@@ -63,15 +88,20 @@ const BloodBank = () => {
 
   return (
     <div>
-      <div className='flex flex-col w-full min-h-screen justify-center items-center'>
-        <div className='flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 w-full max-w-4xl p-8 sm:p-12 rounded-xl overflow-hidden'>
+      <div className="flex flex-col w-full min-h-screen justify-center items-center">
+        <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 w-full max-w-4xl p-8 sm:p-12 rounded-xl overflow-hidden">
           <BankRequestSection
             hospitalRequests={hospitalRequests}
             setHospitalRequests={setHospitalRequests}
-            isBloodDataChanged={isBloodDataChanged}
+            isBloodDataChanged={isBloodDataDecrease}
           />
           <ShowChart bloodData={bloodData} />
         </div>
+        <PendingRequests
+          DonorRequests={DonorRequests}
+          setDonorRequests={setDonorRequests}
+          isBloodDataChanged={isBloodDataIncrease}
+        />
         <RequestDonationModal />
       </div>
     </div>
