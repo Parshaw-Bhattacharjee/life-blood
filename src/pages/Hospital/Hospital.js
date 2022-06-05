0b78de755b94ba4f426df0af9551701349f9bf22
Parshaw Bhattacharjee <parshawbhattacharjee@gmail.com>
2022-06-05 23:43:48 +0530

    fix

--- src/pages/Hospital/Hospital.js
+++ src/pages/Hospital/Hospital.js
@@ -5,8 +5,8 @@ import HospitalRequestSection from "./components/HospitalRequestSection";
 
 const Hospital = () => {
   return (
-    <div className="flex flex-col p-2 justify-center items-center">
-      <div className="flex w-full  justify-center items-center">
+    <div className="flex flex-col min-h-screen p-2 justify-center items-center">
+      <div className="flex w-full justify-center items-center">
         <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 w-full max-w-4xl p-8 sm:p-12 rounded-xl overflow-hidden">
           <HospitalRequestSection />
           <BloodRequestForm />
