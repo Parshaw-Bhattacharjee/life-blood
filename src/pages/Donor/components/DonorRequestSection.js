2387f10e64d02b54172e840972c0d6214b32b0e2
Parshaw Bhattacharjee <parshawbhattacharjee@gmail.com>
2022-06-05 08:32:59 +0530

    Co-authored-by: Aryan Gupta <aguyran@gmail.com>

--- src/pages/Donor/components/DonorRequestSection.js
+++ src/pages/Donor/components/DonorRequestSection.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import { BloodBankRequest } from './BloodBankRequest';
-import { useAuth } from '../../../contexts/auth-context';
-import { db } from '../../../firebase';
+import React from "react";
+import { BloodBankRequest } from "./BloodBankRequest";
+import { useAuth } from "../../../contexts/auth-context";
+import { db } from "../../../firebase";
 import {
   collection,
   doc,
   onSnapshot,
   query,
   updateDoc,
-} from 'firebase/firestore';
+} from "firebase/firestore";
 
 const DonorRequestSection = ({
   setBloodBank,
   bloodBank,
   setAllBloodBankData,
   allBloodBankData,
+  bloodgroup,
 }) => {
   const { user } = useAuth();
 
@@ -27,7 +28,7 @@ const DonorRequestSection = ({
           return el;
         });
       console.log(updatedReq);
-      await updateDoc(doc(db, 'blood_bank', request.userUID), {
+      await updateDoc(doc(db, "blood_bank", request.userUID), {
         donorRequest: updatedReq,
       });
     })();
@@ -60,16 +61,22 @@ const DonorRequestSection = ({
   };
 
   return (
-    <div className='flex flex-col space-y-1 justify-center items-center border-2 border-red-300 shadow-lg rounded-lg'>
-      <div className=''>
-        <h1 className='font-bold text-xl tracking-wide'>Donation Request</h1>
-      </div>
-      <div className='p-2'>
-        <div className='border-2 flex flex-row space-x-2 p-2'>
-          <h1>Blood Bank Name</h1>
-          <h1>Blood Bank Location</h1>
+    <div className="flex flex-col space-y-1 justify-center items-center border-2 border-red-300 shadow-lg rounded-lg relative">
+      {bloodgroup ? (
+        <div className="absolute w-20 h-20 bg-rose-400 rounded-full -right-4 -top-8">
+          <h1 className="font-bold text-3xl p-5">{bloodgroup}</h1>
         </div>
-        <div className='w-full p-2 flex-col'>
+      ) : null}
+      <div className="">
+        <h1 className="font-bold text-xl tracking-wide">Donation Request</h1>
+      </div>
+      <table className="p-2 my-4">
+        <thead className="border-2 flex flex-row space-x-2 p-2">
+          <th>Blood Bank Name</th>
+          <th>Blood Bank Location</th>
+        </thead>
+        <tbody className="w-full p-2 flex-col">
+          {console.log(bloodBank)}
           {bloodBank.map((el) => {
             return (
               <BloodBankRequest
@@ -79,8 +86,8 @@ const DonorRequestSection = ({
               />
             );
           })}
-        </div>
-      </div>
+        </tbody>
+      </table>
     </div>
   );
 };
