a609402f25750bef5991765c4131a3753efa6e9b
Parshaw Bhattacharjee <parshawbhattacharjee@gmail.com>
2022-06-06 02:49:26 +0530

    added fix

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -42,17 +42,6 @@ const Contact = () => {
         >
           <strong class="font-bold">Success!</strong>
           <span class="block sm:inline"> We will reach you soon... </span>
-          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg
-              class="fill-current h-6 w-6 text-green-500"
-              role="button"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <title>Close</title>
-              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-            </svg>
-          </span>
         </div>
       )}
       {status?.type === "error" && (
@@ -62,17 +51,6 @@ const Contact = () => {
         >
           <strong class="font-bold">Error!</strong>
           <span class="block sm:inline"> Kindly try again! </span>
-          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg
-              class="fill-current h-6 w-6 text-red-500"
-              role="button"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <title>Close</title>
-              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-            </svg>
-          </span>
         </div>
       )}
       <div className="flex w-full min-h-screen justify-center items-center">
